a5d78ef6480d744ff5f8935275f6f3ba823b0804
unknown <andryg9501@gmail.com>
2025-07-12 00:27:19 +0700

    add file Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Homepage from './components/Homepage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <h1 className='text-blue-400 text-3xl'>hello world</h1>
-      </div>
+      <Homepage />
     </>
   )
 }
